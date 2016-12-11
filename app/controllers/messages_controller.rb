64529c4889048dee173b3ad7133b43c319187a12
Blake Tan <koonchong@gmail.com>
2016-12-11 14:42:41 -0800

    more syntax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -52,8 +52,7 @@ class MessagesController < ApplicationController
         @client.account.messages.create(
           :from => from,
           :to => person.phone,
-          :body => "Hi, someone near #{@req.address} needs your help. The voter is experiencing this issue: " 
-          + issue +  "\nThey have described the issue as: #{@req.desc}. Please repond with Yes if you are able to help."
+          :body => "Hi, someone near #{@req.address} needs your help. The voter is experiencing this issue: " + issue +  "\nThey have described the issue as: #{@req.desc}. Please repond with Yes if you are able to help."
         )
         puts "Sent request for help to #{person.fname}"
       end
