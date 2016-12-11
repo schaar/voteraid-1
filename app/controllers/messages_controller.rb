8149bf76026878ac89f05f97b3690f8c836f41ab
Blake Tan <koonchong@gmail.com>
2016-12-10 17:14:57 -0800

    fix request optional relationship with responder

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,14 +5,26 @@ class MessagesController < ApplicationController
   require 'json'
   include MessagesHelper
 
+  use Rack::Session::Cookie, :key => 'rack.session',
+                             :path => '/',
+                             :secret => ENV["SECRET_KEY_BASE"]
+
   def reply
     message_body = params["Body"]
     from_number = params["From"]
+    if session["request_id"].nil?
+      @req = Request.create!({phone: from_number, status: 0, responder_id: nil})
+      @message = @req.messages.create({body: message_body})
+    else
+      @req = Request.find(session["request_id"])
+      @message = @req.messages.create({body: message_body})
+    end
+    session["request_id"] = @req.id
     boot_twilio
     sms = @client.messages.create(
       from: ENV["TWILIO_NUMBER"],
       to: from_number,
-      body: "Hello there, thanks for texting me. Your number is #{from_number}."
+      body: "Hello there, thanks for texting me. Your request is #{@req.id} "
     )
     
   end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request < ApplicationRecord
-  belongs_to :responder, inverse_of: :request
+  belongs_to :responder, inverse_of: :requests, optional: true
   has_many :messages, inverse_of: :request
 
 end
